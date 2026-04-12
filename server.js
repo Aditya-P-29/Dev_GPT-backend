9cd38ef62b82d84abe32ca089cf2c6e5d921ca38
Aditya-P-29 <adityapotdar7309@gmail.com>
2026-04-12 11:01:05 +0530

    added a url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "https://devgpt-backend-gkai.onrender.com"
+  origin: "https://devgpt.vercel.app" // your actual Vercel URL
 }))
 
 app.use("/api", chatRoutes);

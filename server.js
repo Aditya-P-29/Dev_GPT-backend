ee0e2c00c2bb888460f1081bcbe8e1c7ff9aea66
Aditya-P-29 <adityapotdar7309@gmail.com>
2026-08-10 16:43:56 +0530

    removed the workflow

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const connectDB = async () => {
 
 const start = async () => {
   await connectDB();
-  app.listen(PORT, "0.0.0.0" ,() => {
+  app.listen(PORT,() => {
     console.log(`server running on ${PORT}`);
   });
 };

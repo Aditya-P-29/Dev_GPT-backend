8d4d6690a54eb66012306599c32fd27c97a23240
Aditya-P-29 <adityapotdar7309@gmail.com>
2026-08-11 19:21:24 +0530

    updated server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const connectDB = async () => {
 
 const start = async () => {
   await connectDB();
-  app.listen(PORT, "0.0.0.0/0" ,() => {
+  app.listen(PORT, "0.0.0.0" ,() => {
     console.log(`server running on ${PORT}`);
   });
 };
